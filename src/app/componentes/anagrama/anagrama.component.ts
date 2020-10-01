f71f4b7cb7f3e6fcf4a8c3b3200eee236985fc69
Ezezsg <ezequielfperez@gmail.com>
2020-09-30 23:00:00 -0300

    anagrama y ppt

--- src/app/componentes/anagrama/anagrama.component.ts
+++ src/app/componentes/anagrama/anagrama.component.ts
@@ -7,9 +7,164 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnagramaComponent implements OnInit {
 
-  constructor() { }
+  palabra_Usuario:string="";
+  palabra_Sistema:string="";
+  palabra_Mezclada:string="";
+  mensaje:string;
+  mensajeFinal:string;
+
+  intentos_Usuario:number=0;
+  intentos_Sistema:number=5;
+  aciertosPositivos:number=0;
+
+  puntos:number;
+
+  empieza:boolean = false;
+  resultado:boolean = false;
+
+  correcto:boolean = false;
+  incorrecto:boolean = false;
+	
+  private ListaDePalabras:Array<string> = [
+    "perro",
+    "persona",
+    "computadora",
+    "guitarra",
+    "gobierno",
+    "liberalismo",
+    "sol",
+    "luna",
+    "libre",
+    "mercado",
+    "música",
+    "tierra",
+    "estructura",
+    "polimorfismo",
+    "herencia"
+   ];
 
   ngOnInit() {
+  }
+
+  constructor()  {
+      
+  }    
+
+  generarJuego()
+  { 
+    this.puntos = 0;
+    this.empieza = true;
+    this.intentos_Usuario = 0;
+    this.mensajeFinal = "";
+    this.obtenerPalabra();
+    this.mensaje = "Escriba la palabra correcta y haga click en 'verificar'";    
+  } 
+
+
+  obtenerPalabra()
+  {
+    if (this.empieza == true)
+    {    
+      if (this.ListaDePalabras.length > 0)
+      {
+        this.ListaDePalabras = this.ListaDePalabras.sort(()=>{ return Math.random() - 0.5});
+        this.palabra_Sistema = this.ListaDePalabras.pop().toUpperCase();
+
+        this.mezclarLetras();
+      }
+    }
+  }
+  
+  
+    mezclarLetras()
+    {
+      do
+      {
+        this.palabra_Mezclada = this.palabra_Sistema.split('').sort(function(){return 0.5 - Math.random() }).join('');
+      } while (this.palabra_Mezclada == this.palabra_Sistema)
+
+      console.log(this.palabra_Sistema);
+      console.log(this.palabra_Mezclada);
+    }
+
+
+
+  verificar()
+  {
+    if (this.intentos_Usuario < this.intentos_Sistema && this.empieza == true)
+    {
+      this.correcto = false;
+      this.incorrecto = false;
+      if(this.palabra_Usuario.toUpperCase() == this.palabra_Sistema.toUpperCase())
+      {
+        this.puntos += 10;
+        this.aciertosPositivos++;
+
+        this.correcto = true;
+        this.mensaje = "Palabra correcta, pruebe con esta otra palabra...";
+      }
+      else
+      {
+        this.incorrecto = true;
+        this.mensaje = "Palabra incorrecta, era: "+ this.palabra_Sistema +", pruebe con esta otra palabra...";    
+           
+      }
+      this.intentos_Usuario++; 
+      this.palabra_Usuario = "";
+      if (this.intentos_Usuario < this.intentos_Sistema)
+        this.obtenerPalabra();    
+
+
+      console.log(this.puntos);
+
+
+      if (this.intentos_Usuario >= this.intentos_Sistema)
+      {this.finDelJuego();} 
+    }
+  }
+
+
+  finDelJuego()
+  {
+    this.empieza = false;
+    this.puntos += this.puntos;
+    //guardo
+
+    
+
+    if (this.aciertosPositivos == this.intentos_Sistema){
+      this.mensajeFinal = "Excelente puntuacion! sumo un total de "+ this.puntos +" Pts. ";
+      this.resultado = true;
+    }
+    else if (this.aciertosPositivos > 0){
+      this.mensajeFinal = "Sumo un total de "+ this.puntos +" Pts. ";
+      this.resultado = true;
+    }
+    else{
+      this.mensajeFinal = "Usted perdio!!! no pudo acertar ninguna palabra. ";
+      this.resultado = false;
+    }
+
+    
+
+      this.mensajeFinal += "Haga click en el boton 'Juego nuevo' para volver a comenzar.";
+
+    this.resetVariables();
+  }
+
+
+
+
+  resetVariables()
+  {
+    this.palabra_Usuario = "";
+    this.palabra_Sistema = "";
+    this.palabra_Mezclada = "";
+
+    this.aciertosPositivos = 0;
+    this.correcto = false;
+    this.incorrecto = false;
+    
   }
 
 }
--- src/app/componentes/piedra-papel-tijera/piedra-papel-tijera.component.ts
+++ src/app/componentes/piedra-papel-tijera/piedra-papel-tijera.component.ts
@@ -7,9 +7,215 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PiedraPapelTijeraComponent implements OnInit {
 
-  constructor() { }
+  jugadaUsuario:string=null;
+  jugadaSistema:string;
+  mensaje:string;
+  mensajeFinal:string;
+
+  aux_jugadaSistema;
+
+  puntos:number;
+
+  empieza:boolean = false;
+  resultado:boolean = false;
+
+  correcto:boolean = false;
+  incorrecto:boolean = false;
+
+  constructor() {
+  	this.prepararJuego();
+  }
 
   ngOnInit() {
+
   }
+
+  prepararJuego()
+  {
+    this.puntos = 0;
+    this.mensaje="Haga click en la alguno de los botones (en la piedra, en el papel o en la tijera) para realizar su jugada.";
+    this.empieza=true;
+    this.jugadaUsuario=null;
+    this.generarJugada();
+  }
+
+
+
+  generarJugada()
+  {
+    //this.aux_jugadaSistema = 3;
+    this.aux_jugadaSistema = Math.floor(Math.random() * ((3+1) - 1)+1);
+    console.log(this.aux_jugadaSistema);    
+
+    switch (this.aux_jugadaSistema)
+    {
+      case 1:
+        this.jugadaSistema = "Piedra";        
+        break;
+
+      case 2:        
+        this.jugadaSistema = "Papel";
+        break;
+
+      case 3:        
+        this.jugadaSistema = "Tijera";
+        break;
+    }
+
+    console.log(this.jugadaSistema);
+
+
+  }
+
+
+
+  verificarJugada()
+  {    
+    if(this.empieza == true)
+    {
+    if(this.jugadaUsuario != this.jugadaSistema)
+    {
+
+      if(this.jugadaUsuario == "Piedra")
+      {
+        if (this.jugadaSistema == "Tijera")
+        {
+          this.resultado = true;
+          
+          this.mensaje = "Usted ha ganado";
+          this.puntos += 10;
+        }
+        else
+        {      
+          this.resultado = false;
+          
+          this.mensaje = "Usted perdio";
+        }
+      }
+
+
+      if(this.jugadaUsuario == "Tijera")
+      {
+        if (this.jugadaSistema == "Papel")
+        {
+          this.resultado = true;
+          
+          this.mensaje = "Usted ha ganado";
+          this.puntos += 10;
+        }
+        else
+        {      
+          this.resultado = false;
+          
+          this.mensaje = "Usted perdio";
+        }
+      }
+      
+
+      if(this.jugadaUsuario == "Papel")
+      {
+        if (this.jugadaSistema == "Piedra")
+        {
+          this.resultado = true;
+          
+          this.mensaje = "Usted ha ganado";
+          this.puntos += 10;
+        }
+        else
+        {      
+          this.resultado = false;
+          
+          this.mensaje = "Usted perdio";
+        }
+      }      
+    }
+    else
+    {
+      this.resultado = false;
+      this.mensaje = "Empate";
+      this.puntos += 5;
+    }
+
+
+    
+    this.mensaje += ". Sumo "+ this.puntos +"Pts., haga click en 'Intentar otra vez' e intente con otra jugada.";
+
+    switch (this.jugadaSistema)
+    {
+      case "Piedra":
+        document.getElementById("BtnPiedra").setAttribute("class", "btn btn-danger active");       
+        break;
+
+      case "Papel":        
+        document.getElementById("BtnPapel").setAttribute("class", "btn btn-danger active");  
+        break;
+
+      case "Tijera":        
+        document.getElementById("BtnTijera").setAttribute("class", "btn btn-danger active");  
+        break;
+    }
+    
+    
+    
+    this.finDelJuego();
+    }
+  }
+
+
+
+QueHay(val)
+{
+  if (this.empieza == true)
+  {
+    this.jugadaUsuario = val.target.alt;
+    console.log(this.jugadaUsuario); 
+
+    this.desmarcarBotones();
+    switch (this.jugadaUsuario)
+    {
+      case "Piedra":
+        document.getElementById("BtnPiedra_user").setAttribute("class", "btn btn-primary active");   
+        break;
+
+      case "Papel":
+        document.getElementById("BtnPapel_user").setAttribute("class", "btn btn-primary active");   
+        break;
+
+      case "Tijera":    
+        document.getElementById("BtnTijera_user").setAttribute("class", "btn btn-primary active"); 
+        break;
+    }
+  }
+
+  this.verificarJugada();
+}
+
+
+
+desmarcarBotones()
+{
+  document.getElementById("BtnPiedra_user").setAttribute("class", "btn btn-outline-primary");  
+  document.getElementById("BtnPapel_user").setAttribute("class", "btn btn-outline-primary");
+  document.getElementById("BtnTijera_user").setAttribute("class", "btn btn-outline-primary"); 
+
+  document.getElementById("BtnPiedra").setAttribute("class", "btn btn-outline-danger");  
+  document.getElementById("BtnPapel").setAttribute("class", "btn btn-outline-danger");
+  document.getElementById("BtnTijera").setAttribute("class", "btn btn-outline-danger");
+}
+
+
+finDelJuego()
+{
+  this.empieza = false;
+  //guardo
+  
+}
+
+
+reiniciarJuego()
+{
+  this.prepararJuego();
+  this.desmarcarBotones();
+}
 
 }
